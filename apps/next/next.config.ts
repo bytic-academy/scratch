76c0cc8e0f926e87a47a562df0c3c92079b5eee2
Arash <arash382.jb@gmail.com>
2025-10-25 21:01:59 +0330

    fix: add @prisma/nextjs-monorepo-workaround-plugin

--- apps/next/next.config.ts
+++ apps/next/next.config.ts
@@ -3,12 +3,20 @@
  * for Docker builds.
  */
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 import "./src/env";
 
 const config: NextConfig = {
   output: "standalone",
   serverExternalPackages: ["@turbowarp/packager", "dockerode"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default config;
